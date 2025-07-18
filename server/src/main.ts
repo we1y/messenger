b97ad5d8fbb214c4a3f0855af687ad687b04cfcc
we1y <den.kyl.2003@gmail.com>
2025-07-18 23:44:06 +0300

    +swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors();
+
+  const config = new DocumentBuilder()
+      .setTitle('Messenger API')
+      .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
