812eb72113546302d1205b9f1f48f63c2ab203e3
we1y <den.kyl.2003@gmail.com>
2025-07-19 23:34:14 +0300

    update, add client

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from "./dto/create-user.dto";
 
